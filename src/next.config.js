c9729d958a6c8da864e4e985198ceb5268b972c5
Fogrexon <fogrexon@gmail.com>
2020-08-30 16:35:18 +0900

    install eslint

--- src/next.config.js
+++ src/next.config.js
@@ -1,3 +1,18 @@
+import webpack from 'webpack'
+import dotenv from 'dotenv'
+dotenv.config();
+
+
 module.exports = {
   distDir: '../.next',
+  webpack: config => {
+    const env = Object.keys(process.env).reduce((acc, curr) => {
+      acc[`process.env.${curr}`] = JSON.stringify(process.env[curr]);
+      return acc;
+    }, {});
+
+    config.plugins.push(new webpack.DefinePlugin(env));
+
+    return config;
+  },
 }
